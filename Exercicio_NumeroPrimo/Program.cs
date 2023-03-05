2ec4ac291211c56ee1b9b7e7185a7c953c6cfbc2
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-05 15:07:18 -0300

    Add project files.

---
+++ Exercicio_NumeroPrimo/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //exercicio numero primo
+
+        double num;
+        int cont=0;
+      
+        Console.WriteLine("digite um numero: ");
+        num = int.Parse(Console.ReadLine());
+           
+        if (num > 0)
+        {
+
+            for (int i = 1; i <= num; i++)
+            {
+                    
+                if ((num%i) == 0)
+                {
+                    cont++;
+                }
+                if (cont > 2)
+                {
+                    break;
+                }
+                
+            }
+                
+            if (cont > 2)
+            {
+                Console.WriteLine("numero nao primo");
+            }
+            else
+            {
+                Console.WriteLine("numero primo");
+            }
+        }
+    }
+}
